a0dde818e82d87108c62cca24e1ad2c5e27b7356
muratkilic1978 <muratkilic1978@gmail.com>
2018-02-05 12:08:19 +0100

    My first PHP script
    I have uploaded my first PHP script.

---
+++ intro-php/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test PHP</title>
+</head>
+<body>
+   <h1><?php echo "Murat Kilic"; ?></h1>
+   <h2><?php print "My favourite color is green"; ?></h2>
+   
+    <?php
+        //Single line comment in PHP
+        /* Multi-line comments in PHP
+        *
+        *
+        */
+        # This is also a single line comment in PHP
+        $firstname1 = "Fani";
+        $firstname2 = "Mark";
+        $firstname3 = "Kamilla";
+        $firstname4 = "Christian";
+    ?>
+    <h1><?php echo $firstname1; ?></h1>
+    <h1><?php echo $firstname2; ?></h1>
+    <h1><?php echo $firstname3; ?></h1>
+    <h1><?php echo $firstname4; ?></h1>
+    
+    
+    
+    
+    
+    
+    
+    
+</body>
+</html>
