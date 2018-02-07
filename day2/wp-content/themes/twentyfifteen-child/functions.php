68389e8c27ad323ee6c4397387e3ec52c29bee03
muratkilic1978 <muratkilic1978@gmail.com>
2018-02-07 19:56:10 +0100

    I have uploaded a WordPress child-theme
    This WordPress child-theme was made by Fani and uploaded by Murat and serves as an example of how to setup your own child-theme.

---
+++ day2/wp-content/themes/twentyfifteen-child/functions.php
@@ -0,0 +1,20 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'template_files' );
+function template_files() {
+   wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/rtl.css' );
+
+}
+
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+?>
