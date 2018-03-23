851bae40f5589b2a899f50a7b71e645c3478a960
muratkilic1978 <muratkilic1978@gmail.com>
2018-03-23 12:11:50 +0100

    I have upload ned HTML project with POST and GET method
    I have upload ned HTML project with POST and GET method

---
+++ day6/post-and-get-method/collect.php
@@ -0,0 +1,11 @@
+<?php
+
+{
+	echo 'The following string was sent from form #1: ' . $_POST['email'];
+} 
+
+{
+	echo 'The following string was sent from form #2: ' . $_GET['data'];
+}
+
+?>
---
+++ day6/post-and-get-method/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title></title>
+	<meta charset="utf-8">
+
+</head>
+<body>
+	<form method="post" action="collect.php">
+		<h1>Form #1</h1>
+		<input type="email" name="email" placeholder="enter a string">
+		<button type="submit">submit</button>
+	</form>
+	<form method="get" action="collect.php">
+		<h1>Form #2</h1>
+		<input type="text" name="email" placeholder="enter a string">
+		<button type="submit">submit</button>
+	</form>
+	
+</body>
+
+</html>
